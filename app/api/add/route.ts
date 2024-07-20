c74128262ea76021f882085f34acc343e7573cb3
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-07-21 01:04:44 +0200

    many jobs

--- app/api/add/route.ts
+++ app/api/add/route.ts
@@ -2,30 +2,37 @@ export const dynamic = "force-dynamic"; // defaults to auto
 
 export const POST = async (request: Request) => {
   const body = await request.json();
-  const name = body.name;
+  const count = body.count || 1;
 
-  console.log(`Job received: ${name}`);
-  const payload = {
-    email: process.env.EZQ42_EMAIL,
-    payload: {
-      url: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/run`,
-      method: "POST",
-      body: {
-        name,
-        date: new Date().toISOString(),
+  const payloads = [];
+  for (let i = 0; i < count; i++) {
+    const name = `Name ${i}`;
+    payloads.push({
+      email: process.env.EZQ42_EMAIL,
+      payload: {
+        url: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/run`,
+        method: "POST",
+        body: {
+          name,
+          date: new Date().toISOString(),
+        },
       },
-    },
-  };
-  console.log(payload);
-  const url = `${process.env.EZQ42_URL}/api/v1/job`;
-  console.log(`Sending job to ${url}`);
-  await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
+    });
+  }
+
+  await Promise.all(
+    payloads.map(async (payload) => {
+      console.log(`Sending job to ${payload.payload.url}`);
+      const url = `${process.env.EZQ42_URL}/api/v1/job`;
+      await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+    })
+  );
 
   return new Response("ok", {
     status: 200,
